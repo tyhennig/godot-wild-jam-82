8aa94fa51180688a9fe167b630a6cbc381cc246f
Tyler Hennig <tyhennig@gmail.com>
2025-06-17 22:51:30 -0600

    Moved Action Manager logic to GameState Singleton

---
+++ scenes/EnemyPlacer.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+/// <summary>
+/// Contains the logic / algorithm for deciding how many and where to place enemy ships on the grid
+/// </summary>
+public partial class EnemyPlacer : Node
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public void PlaceEnemyShips(int roundNumber)
+	{
+		
+	}
+}
